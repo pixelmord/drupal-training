5a5b88422d769e5e06b91bb51ba37f2967b724a7
Reinhard Gloggengiesser <reinhard@Reinhards-MacBook-Air.local>
2013-05-06 17:14:31 +0200

    Added installation of zip program and fixed wrong paths for modules directory

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 echo "[INFO] hello, training setup is starting"
 
 WORKDIR=`pwd`
+echo "[INFO] installing zip"
+apt-get install zip -y
 
 echo "[INFO] downloading drupal with drush to htdocs"
 drush make drupal-training.make htdocs
@@ -12,7 +14,7 @@ cp -a $WORKDIR/code/profiles/ $WORKDIR/htdocs/profiles/
 
 echo "[INFO] copy training modules"
 mkdir -p $WORKDIR/htdocs/sites/all/modules/custom
-cp -a $WORKDIR/code/modules/ $WORKDIR/htdocs/sites/all/modules/custom/
+cp -a $WORKDIR/code/modules/* $WORKDIR/htdocs/sites/all/modules/custom/
 
 echo "[INFO] copy training themes"
 cp -a $WORKDIR/code/themes/ $WORKDIR/htdocs/sites/all/themes/
